7f4def46784a785bc63e52100ed3bac71a8bc3c0
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-01-12 19:40:54 +1100

    Minor enhancements for comment spacing.

--- SumDigitsString/DigitsSumString.cs
+++ SumDigitsString/DigitsSumString.cs
@@ -9,10 +9,10 @@ namespace SumDigitsString
     public class DigitsSumString
     {
         //Given a string, write a method which returns sum of all digits in that string. Assume that string contains only single digits.
-//        Expected input and output
-//SumDigitsInString("1q2w3e") → 6
-//SumDigitsInString("L0r3m.1p5um") → 9
-//SumDigitsInString("") → 0
+        //Expected input and output
+        //SumDigitsInString("1q2w3e") → 6
+        //SumDigitsInString("L0r3m.1p5um") → 9
+        //SumDigitsInString("") → 0
         public void SumOfDigitsString()
         {
             Console.WriteLine("Type your alphanumeric phrase here: ");
