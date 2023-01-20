2c7e83a0a3615e9e52f29fa01b6a4b8b45289e76
Helaman Del Castillo <helaman.delcastillo@praemium.com>
2023-01-21 10:49:33 +1100

    Practice exercise to print double letters.

---
+++ DoubleLetters/DoubledLetters.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DoubleLetters
+{
+    public class DoubledLetters
+    {
+        //Write a query that returns double letters sequence in format: AA AB AC ... ZX ZY ZZ
+        //Expected input and output
+        //(no input) → "AA AB AC ... AZ BA BB ... ZX ZY ZZ"
+        //https://csharpexercises.com/linq/exercise/double-letters#
+        public void DoubledLettersDisplay()
+        {
+            List<string> letters = new List<string>();
+            letters.Add(MainLetters.A.ToString());
+            letters.Add(MainLetters.B.ToString());
+            letters.Add(MainLetters.C.ToString());
+
+            foreach (var item in letters)
+            {
+                for (int i = 0; i < letters.Count; i++)
+                {
+                    Console.Write($"{item}{letters[i]} ");
+                }
+            }
+            Console.ReadLine();
+        }
+        enum MainLetters
+        {
+            A,
+            B,
+            C
+        }
+    }
+}
